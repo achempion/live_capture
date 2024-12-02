612e999eb723c0214c45dcd328381e3aa8259073
Boris Kuznetsov <me@achempion.com>
2024-12-02 02:02:19 +0100

    Add support for more types

--- lib/live_capture/live/component/components/attribute.ex
+++ lib/live_capture/live/component/components/attribute.ex
@@ -5,8 +5,11 @@ defmodule LiveCapture.Component.Components.Attribute do
   attr :attrs, :list,
     examples: [
       [
-        %{name: :name1, type: :string, opts: [examples: ["Your name"]]},
-        %{name: :name2, type: :string, opts: [examples: ["Your name"]]}
+        %{name: :string, type: :string, opts: [examples: ["Your name"]]},
+        %{name: :atom, type: :atom, opts: [examples: [:atom]]},
+        %{name: :boolean, type: :boolean, opts: [examples: [:atom]]},
+        %{name: :integer, type: :integer, opts: [examples: [42]]},
+        %{name: :float, type: :float, opts: [examples: [42.43]]}
       ]
     ]
 
@@ -27,16 +30,57 @@ defmodule LiveCapture.Component.Components.Attribute do
   capture()
 
   def show(%{attr: %{type: :string}} = assigns) do
-    value = Keyword.get(assigns.attr[:opts] || [], :examples, []) |> List.first()
-
-    assigns = assign(assigns, value: value)
+    assigns = set_value(assigns)
 
     ~H"""
-    <div class="flex gap-4 items-center">
-      <div><%= @attr.name %></div>
-      <div class="border bg-gray-200 px-3 py-1 rounded-xl"><%= @attr.type %></div>
+    <.attr_row name={@attr.name} type={@attr.type}>
       <input class="border py-1 px-2 rounded" name={@attr.name} value={@value} />
-    </div>
+    </.attr_row>
+    """
+  end
+
+  def show(%{attr: %{type: :atom}} = assigns) do
+    assigns = set_value(assigns)
+
+    ~H"""
+    <.attr_row name={@attr.name} type={@attr.type}>
+      <input class="border py-1 px-2 rounded" name={@attr.name} value={@value} />
+    </.attr_row>
+    """
+  end
+
+  def show(%{attr: %{type: :boolean}} = assigns) do
+    assigns = set_value(assigns)
+
+    ~H"""
+    <.attr_row name={@attr.name} type={@attr.type}>
+      <input type="radio" id="yes" name={@attr.name} value="true" class="mr-2" />
+      <label for="yes">true</label>
+      <input type="radio" id="no" name={@attr.name} value="false" class="ml-4 mr-2" />
+      <label for="no">false</label>
+      <input type="radio" id="nil" name={@attr.name} value="false" class="ml-4 mr-2" />
+      <label for="nil">nil</label>
+    </.attr_row>
+    """
+  end
+
+  def show(%{attr: %{type: :integer}} = assigns) do
+    assigns = set_value(assigns)
+
+    ~H"""
+    <.attr_row name={@attr.name} type={@attr.type}>
+      <input class="border py-1 px-2 rounded" name={@attr.name} value={@value} />
+    </.attr_row>
+    """
+  end
+
+  def show(%{attr: %{type: :float}} = assigns) do
+    assigns = set_value(assigns)
+
+    ~H"""
+    <.attr_row name={@attr.name} type={@attr.type}>
+      <input class="border py-1 px-2 rounded" name={@attr.name} value={@value} />
+    </.attr_row>
     """
   end
 
@@ -50,5 +94,21 @@ defmodule LiveCapture.Component.Components.Attribute do
     ~H"""
     Unsupported type: `<%= inspect(@attr[:type]) %>`
     """
+  end
+
+  defp attr_row(assigns) do
+    ~H"""
+    <div class="flex gap-4 items-center">
+      <div><%= @name %></div>
+      <div class="border bg-gray-200 px-3 py-1 rounded-xl"><%= @type %></div>
+      <%= render_slot(@inner_block) %>
+    </div>
+    """
+  end
+
+  defp set_value(assigns) do
+    value = Keyword.get(assigns.attr[:opts] || [], :examples, []) |> List.first()
+
+    assign(assigns, value: value)
   end
 end
